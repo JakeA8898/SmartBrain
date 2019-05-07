535987c61ca10cf5192ab74ce7629707b7e75d4a
Jacob <j.anderson98@hotmail.com>
2019-05-07 15:51:51 -0400

    layout and components of application

---
+++ facereconitionbrain/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+import Particles from 'react-particles-js';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }    
+}
+
+function App() {
+  return (
+    <div className="App">
+        <Particles className='particles' 
+            params={particleOptions} />
+        
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/* <FaceRecognition /> */}
+    </div>
+  );
+}
+
+export default App;
