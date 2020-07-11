5f72da87474349995f82c9bcd2eceb1d29497bb6
JakeA8898 <j.anderson98@hotmail.com>
2020-07-11 17:45:11 -0400

    Connected server to postgres db

--- facereconitionapi/server.js
+++ facereconitionapi/server.js
@@ -2,6 +2,15 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'PasswordHere',
+        database: 'smartbrain'
+    }
+});
 
 const app = express();
 app.use(bodyParser.json())
